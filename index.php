ed242276b7d45fea456f5885e249e76a81e226ad
thefallingboy <thefallingboystudio@gmail.com>
2019-10-06 12:27:04 -0500

    Creación de los primeros archivos
    Archivos basicos del tema

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""></meta>
+    <meta name="author" content=""></meta>
+
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory');?>/style.css">
+</head>
+<body>
+    
+</body>
+</html>
